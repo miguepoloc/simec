dbe273080539d996af96b23098be8fa77b1dfb8d
miguepoloc <miguepoloc@gmail.com>
2021-05-27 20:27:08 -0500

    Mejoras a equipos añadidos y campos de personal

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from ..api.models import Zona, Personal, Salidas_De_Campo, Calendario_Salidas_De_Campo
+from ..api.models import Equipo, Zona, Personal, Salidas_De_Campo, Calendario_Salidas_De_Campo, Equipo
 
 
 def index(request):
 
     zona = Zona.objects.all()
     personal = Personal.objects.all()
+    equipo = Equipo.objects.all()
     salida = Salidas_De_Campo.objects.all()
     calendario = Calendario_Salidas_De_Campo.objects.all()
 
-    return render(request, "main/index.html", {'zona_list': zona, 'personal_list': personal, 'salida_de_campo_list': salida, 'calendario_list': calendario})
+    return render(request, "main/index.html", {'zona_list': zona, 'personal_list': personal, 'equipo_list': equipo, 
+        'salida_de_campo_list': salida, 'calendario_list': calendario})
